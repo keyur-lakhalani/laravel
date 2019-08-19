6927f4e705bd97956f506999cfd519ee4e96aac4
keyur-lakhalani <keyur_lakhlani@yahoo.co.in>
2019-08-19 21:25:04 +0530

    Poll first application welcome and question set up

--- resources/views/questions.blade.php
+++ resources/views/questions.blade.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Poll application</title>
-
-        
     </head>
     <body>
         <div>
-           <h1>Welcome to poll application!!!</h1>     
+           <h1>Questions!!!</h1>     
         </div>
     </body>
 </html>
