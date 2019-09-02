bce1f9f2c3b672a517a4388fb3af61d6fcbf2684
Keyur Lakhlani <keyur.lakhlani@gatewaygroup.com>
2019-09-02 15:02:55 +0530

    Questions link in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,6 +50,9 @@
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register Other User') }}</a>
                                 </li>
                             @endif
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('questions.index') }}">{{ __('Questions') }}</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
